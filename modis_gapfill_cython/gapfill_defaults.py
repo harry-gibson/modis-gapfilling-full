901c80db6d118a66c5daba956f45c71197a440e2
harry-gibson <harry.s.gibson@gmail.com>
2020-02-14 17:27:55 +0000

    Added default configs for MAP gapfilling and made names more self-explanatory

--- modis_gapfill_cython/gapfill_defaults.py
+++ modis_gapfill_cython/gapfill_defaults.py
@@ -1,9 +1,9 @@
-from gapfill_config import *
+import gapfill_config
 
 # Temporary file to run the code prior to developing a read-at-runtime config system
 
 # The flags are set / checked using bitwise operators so should be powers of two
-DefaultFlagValues = FlagItems(
+DefaultFlagValues = gapfill_config.FlagItems(
     OCEAN=1,
     FAILURE=2,
     EXTREME=4,
@@ -13,32 +13,75 @@ DefaultFlagValues = FlagItems(
     A2_FILLED=64,
     CLIPPED=128)
 
+DefaultDataConfig_LST = gapfill_config.DataCharacteristicsConfig(
+    CEILING_VALUE = 100.0,
+    FLOOR_VALUE = -100.0,
+    FLOOR_CEILING_ZSCORE = 2.58,
+    CORRECTION_OFFSET = 0,
+    NODATA_VALUE = -9999,
+    ABSOLUTE_ZERO_OFFSET = -273.15)
 
+DefaultDataConfig_EVI = gapfill_config.DataCharacteristicsConfig(
+    CEILING_VALUE = 1.0,
+    FLOOR_VALUE = 0.0,
+    FLOOR_CEILING_ZSCORE = 2.58,
+    CORRECTION_OFFSET = 0,
+    NODATA_VALUE = -9999,
+    ABSOLUTE_ZERO_OFFSET = 0)
 
-DataSpecificDefaultConfig = DataCharacteristicsConfig(
-    CEILING_VALUE=2.0,
-    FLOOR_VALUE=-1.0,
-    FLOOR_CEILING_ZSCORE=2.58,
-    CORRECTION_OFFSET=0,
-    NODATA_VALUE=-9999,
-    ABSOLUTE_ZERO_OFFSET=0)
+DefaultDataConfig_TCW = gapfill_config.DataCharacteristicsConfig(
+    CEILING_VALUE = 2.0,
+    FLOOR_VALUE = -1.0,
+    FLOOR_CEILING_ZSCORE = 2.58,
+    CORRECTION_OFFSET = 0,
+    NODATA_VALUE = -9999,
+    ABSOLUTE_ZERO_OFFSET = 0)
 
-# The despackle and gapfill algorithms search for neighbour cells in an outward spiral i.e. an
+DefaultDataConfig_TCB = gapfill_config.DataCharacteristicsConfig(
+    CEILING_VALUE = 2.0,
+    FLOOR_VALUE = -1.0,
+    FLOOR_CEILING_ZSCORE = 2.58,
+    CORRECTION_OFFSET = 0,
+    NODATA_VALUE = -9999,
+    ABSOLUTE_ZERO_OFFSET = 0)
+
+# The despeckle and gapfill algorithms search for neighbour cells in an outward spiral i.e. an
 # increasing search radius until enough neighbours are found for the checks or until the maximum radius is reached.
 # Define the size of the search and the number of neighbours required. For A1 this determines the fill radius i.e.
 # the size of gap that can be filled.
-DespeckleSpiralDefaultConfig = SpiralSearchConfig(MAX_NBRS_TO_SEARCH=3142, MIN_REQUIRED_NBRS=320, MAX_USED_NBRS=640)
-A1SpiralDefaultConfig = SpiralSearchConfig(MAX_NBRS_TO_SEARCH=3142, MIN_REQUIRED_NBRS=480, MAX_USED_NBRS=960)
-A2SpiralDefaultConfig = SpiralSearchConfig(MAX_NBRS_TO_SEARCH=8, MIN_REQUIRED_NBRS=1, MAX_USED_NBRS=8)
+# All MODIS gapfilling done in MAP since 2014 has used the following parameters which give an effective search
+# radius of approx 31 pixels (i.e. sqrt(3142/pi)) and
+DefaultDespeckleSpiralConfig = gapfill_config.SpiralSearchConfig(
+    MAX_NBRS_TO_SEARCH = 3142,
+    MIN_REQUIRED_NBRS = 320,
+    MAX_USED_NBRS = 640)
 
-DespeckleThresholdDefaultConfig = SpeckleSelectionConfig(SPIRAL=DespeckleSpiralDefaultConfig,
-                                                         EXTREME_BEYOND_SD=2.58, SPECKLE_BEYOND_SD=1.64,
-                                                         SPECKLE_NBR_Z_THRESH=0.2)
+DefaultA1SpiralConfig = gapfill_config.SpiralSearchConfig(
+    MAX_NBRS_TO_SEARCH = 3142,
+    MIN_REQUIRED_NBRS = 480,
+    MAX_USED_NBRS = 960)
 
-A1DefaultParams = A1SearchConfig(SPIRAL=A1SpiralDefaultConfig,
-                                 FILL_GENERATION_METHOD="DIFFERENCE", MAX_ALLOWABLE_RATIO=5.0,
-                                 TRIM_FULL_OUTLIERS=True)
+# for A2 we only normally search the immediate neighbours (as the subsequent steps use previous step outputs,
+# so values are smeared across anyway), but this does not need to be the case
+DefaultA2SpiralConfig = gapfill_config.SpiralSearchConfig(
+    MAX_NBRS_TO_SEARCH = 8,
+    MIN_REQUIRED_NBRS = 1,
+    MAX_USED_NBRS = 8)
 
-A2DefaultParams = A2SearchConfig(SPIRAL=A2SpiralDefaultConfig,
-                                 PASS_AVERAGE_TYPE="MEAN", FILL_GENERATION_METHOD="DIFFERENCE",
-                                 MAX_ALLOWABLE_RATIO=5.0)
+DefaultDespeckleConfig = gapfill_config.DespeckleConfig(
+    SPIRAL = DefaultDespeckleSpiralConfig,
+    EXTREME_BEYOND_SD = 2.58,
+    SPECKLE_BEYOND_SD = 1.64,
+    SPECKLE_NBR_Z_THRESH = 0.2)
+
+DefaultA1Config = gapfill_config.A1SearchConfig(
+    SPIRAL = DefaultA1SpiralConfig,
+    FILL_GENERATION_METHOD = "DIFFERENCE",
+    MAX_ALLOWABLE_RATIO = 0,
+    TRIM_FULL_OUTLIERS = True)
+
+DefaultA2Config = gapfill_config.A2SearchConfig(
+    SPIRAL = DefaultA2SpiralConfig,
+    PASS_AVERAGE_TYPE = "MEAN",
+    FILL_GENERATION_METHOD = "DIFFERENCE",
+    MAX_ALLOWABLE_RATIO = 0)
